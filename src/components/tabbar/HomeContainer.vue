f36d10799cd3e0a14392565925e55b2c4528f2b3
liujing <liujing@1976808188@qq.com>
2019-10-28 17:38:05 +0800

    加了空格

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -12,7 +12,7 @@
 		-->
 	<!--	HomeContainer.vue-->
 		
-		<mt-swipe :auto="4000">
+		   <mt-swipe :auto="4000">
 		 <!-- <mt-swipe-item>1</mt-swipe-item>
 		  <mt-swipe-item>2</mt-swipe-item>
 		  <mt-swipe-item>3</mt-swipe-item>-->
@@ -20,7 +20,7 @@
 		  -->
 		  <mt-swipe-item v-for="item in lunbotuList" :key="item.url">
 		  	
-		  	   <img  :src="item.img">
+		  	     <img  :src="item.img">
 		  	
 		  </mt-swipe-item>
 		</mt-swipe>
